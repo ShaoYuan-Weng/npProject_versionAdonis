a7268e4671ee5bef4883475d128a709445ca02a5
ShaoYuan <shaoyuan.weng@gmail.com>
2019-12-16 13:48:41 +0100

    bigger title

--- public/news.js
+++ public/news.js
@@ -1,4 +1,5 @@
-/*let articles = document.getElementById('articles')
+/*
+let articles = document.getElementById('articles')
 
 fetch('https://newsapi.org/v2/everything?q=handicraft&apiKey=5e6d33528ade4d2d9e262df704ed8f09')
   .then(function (response) {
@@ -10,28 +11,29 @@ fetch('https://newsapi.org/v2/everything?q=handicraft&apiKey=5e6d33528ade4d2d9e2
       articles.innerHTML +=
         `<div class="col-sm-6 col-md-4 item"><a href="${data.articles[i].url}"><img class="img-fluid" src="${data.articles[i].urlToImage}"></a><h3 class="name">${data.articles[i].title}</h3><p class="description">${data.articles[i].description.substring(0, 255)}...</p></div>`
     }
-  })*/
+  })
+  */
 
-  new Vue({
-    el: '#app',
-    data: {
-      articles: [],
-      oldArticles: [],
-      input: ""
-    },
-    watch: {
-      input: function() {
-        if (this.input === "") {
-          this.clearInput()
-        } else {
-          this.articles = this.articles.filter((item) => item.title.toLowerCase().includes(this.input.toLowerCase()))
-        }
+new Vue ({
+  el: '#app',
+  data: {
+    articles: [],
+    oldArticles: [],
+    input: ''
+  },
+  watch: {
+    input: function () {
+      if (this.input === "") {
+        this.clearInput()
+      } else {
+        this.articles = this.articles.filter((item) => item.title.toLowerCase().includes(this.input.toLowerCase()))
       }
-    },
-    created() {
-      axios.get('https://newsapi.org/v2/everything?q=handicraft&apiKey=5e6d33528ade4d2d9e262df704ed8f09')
+    }
+  },
+  created () {
+    axios.get('https://newsapi.org/v2/everything?q=handicraft&apiKey=5e6d33528ade4d2d9e262df704ed8f09')
       .then((response) => {
-        for (let i=0; i<response.data.articles.length; i++) {
+        for (let i = 0; i < response.data.articles.length; i++) {
           let article = {}
           const img = response.data.articles[i].urlToImage === null ? "blank.jpg" : response.data.articles[i].urlToImage
           article.url = response.data.articles[i].url
@@ -42,10 +44,10 @@ fetch('https://newsapi.org/v2/everything?q=handicraft&apiKey=5e6d33528ade4d2d9e2
           this.oldArticles.push(article)
         }
       })
-    }, 
-    methods: {
-      clearInput: function() {
-        this.articles = this.oldArticles
-      }
+  },
+  methods: {
+    clearInput: function () {
+      this.articles = this.oldArticles
     }
-  })
+  }
+})
