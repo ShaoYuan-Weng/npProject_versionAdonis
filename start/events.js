72adc8880f9eaa6942f466facdee28b8a6bf5f31
ShaoYuan <shaoyuan.weng@gmail.com>
2019-12-19 15:49:16 +0100

    event

--- start/events.js
+++ start/events.js
@@ -2,8 +2,8 @@ const Event = use('Event')
 const Mail = use('Mail')
 
 Event.on('forgot::password', async (data) => {
-  /*await Mail.send('resetPasswordEmail', ({ token: data.token }), (message) => {
+  await Mail.send('resetPasswordEmail', ({ token: data.token }), (message) => {
     message.to('by04551@gmail.com')
-    message.from('from@email')
-  })*/
+    message.from('DesignNet@email.com')
+  })
 })
